a8a18888abe6ab03643015658c48f7697b51a247
compeng-utp <56282056+compeng-utp@users.noreply.github.com>
2020-02-26 23:09:04 +0800

    Add files via upload

--- instagram.py
+++ instagram.py
@@ -4,10 +4,12 @@ from bs4 import BeautifulSoup
 
 tag=sys.argv[-1]
 url = "https://www.instagram.com/explore/tags/" + str(tag)
-source_code = requests.get(url)
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
+headers = {'User-Agent': user_agent}
+source_code = requests.get(url, headers=headers)
 plain_text = source_code.text
 
-soup = BeautifulSoup(plain_text)
+soup = BeautifulSoup(plain_text, "html.parser")
 # Extract total number of posts in this hashtag
 nposts = soup.find('span', {'class': 'g47SY'}).text
  
