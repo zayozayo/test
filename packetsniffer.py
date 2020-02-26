ad3737b0d60d9bc641da1754ccf02348cf53889d
compeng-utp <56282056+compeng-utp@users.noreply.github.com>
2020-02-26 20:29:09 +0800

    Add files via upload

--- packetsniffer.py
+++ packetsniffer.py
@@ -1,7 +1,7 @@
 
+import socket
 from struct import *
 
-#create an INET, STREAMing socket
 try:
 	s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 except socket.error , msg:
@@ -9,36 +9,21 @@ except socket.error , msg:
 	sys.exit()
 
 # receive a packet
-while True:
-	packet = s.recvfrom(65565)
+
+packet = s.recvfrom(65565)
 	
-	#packet string from tuple
-	packet = packet[0]
+#packet string from tuple
+packet = packet[0]
 	
-	#take first 20 characters for the ip header
-	ip_header = packet[0:20]
+#take first 20 characters for the ip header
+ip_header = packet[0:20]
 	
-	#now unpack them :)
-	iph = unpack('!BBHHHBBH4s4s' , ip_header)
+#now unpack them :)
+iph = unpack('!BBHHHBBH4s4s' , ip_header)
 	
-	version_ihl = iph[0]
-	ttl = iph[5]
-	protocol = iph[6]
-	s_addr = socket.inet_ntoa(iph[8]);
-	d_addr = socket.inet_ntoa(iph[9]);
+protocol = iph[6]
+s_addr = socket.inet_ntoa(iph[8]);
+d_addr = socket.inet_ntoa(iph[9]);
 	
-	print ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr)
-	
-	tcp_header = packet[iph_length:iph_length+20]
-	
-	#now unpack them :)
-	tcph = unpack('!HHLLBBHHH' , tcp_header)
-	
-	source_port = tcph[0]
-	dest_port = tcph[1]
-	sequence = tcph[2]
-	acknowledgement = tcph[3]
-	doff_reserved = tcph[4]
-	
-	print 'Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) 
-	
+print ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr)
+		
